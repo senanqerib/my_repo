dc262e9af72a532bee4ba114ff89c59dedbd09f7
Acer <Acer@192.168.77.1>
2017-10-13 01:00:31 +0400

    Added File Uploading (Servlet)-PATH

--- src/java/UploadServlet.java
+++ src/java/UploadServlet.java
@@ -6,10 +6,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
  
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import readcert.ReadCert;
 
 public class UploadServlet extends HttpServlet {
    
@@ -91,6 +93,9 @@ public class UploadServlet extends HttpServlet {
                }
                fi.write( file ) ;
                out.println("Uploaded Filename: " + file + "<br>");
+               ReadCert.Read_PEM_DER_Cert(file.toString());
+               
+               
             }
          }
          out.println("</body>");
